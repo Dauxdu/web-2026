8bb8450586ac8a132d2ee1c3c1b8a118242b5c28
Dauxdu <dauxdu@yandex.ru>
2026-09-21 15:19:41 +0400

    fix(tests): remove executablePath from puppeteer launch options

--- Lab1/Задание 1/src/App.test.ts
+++ Lab1/Задание 1/src/App.test.ts
@@ -6,7 +6,6 @@ let browser: Browser;
 beforeAll(async () => {
     browser = await puppeteer.launch({
         headless: true,
-        executablePath: '/usr/bin/chromium',
         args: ['--no-sandbox'],
     });
 });
